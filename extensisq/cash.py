f70c38e99b840eb45c272a0b8a843cd519923910
WRKampi <WRKampi@users.noreply.github.com>
2020-11-17 18:29:38 +0100

    derived methods
    and bug fixes

--- extensisq/cash.py
+++ extensisq/cash.py
@@ -1,10 +1,11 @@
 import numpy as np
-from scipy.integrate._ivp.rk import RungeKutta, RkDenseOutput, norm
+from scipy.integrate._ivp.rk import RungeKutta, RkDenseOutput, norm, rk_step
 
 
 SAFETY = 0.9
 MIN_FACTOR = 1/5
-MAX_FACTOR = 5.
+MAX_FACTOR = 5
+
 
 
 class CK45(RungeKutta):
@@ -17,6 +18,9 @@ class CK45(RungeKutta):
     derivative function. After the first failed prediction, propagation with 
     fallback solutions of reduced oredr and step size is assessed. These 
     fallback solutions do not need extra derivative evaluations.
+    
+    A fixed order (4,5) method with the Cash Karp parameters is available as
+    CK45_o.
     
     Can be applied in the complex domain.
 
@@ -161,13 +165,17 @@ class CK45(RungeKutta):
     P_fallback[:,0,:]  += [1, -2,  1]                 # derivative at start
     P_fallback[:,-1,:] += [0, -1,  1]                 # derivative at end
     
+    def __init__(self, fun, t0, y0, t_bound, **extraneous):
+        super(CK45, self).__init__(fun, t0, y0, t_bound, **extraneous)
+        # prevent possible nan or inf in K that may interfere with interpolator:
+        self.K[:,:] = 0.
     
     def _step_impl(self):
         t = self.t
         y = self.y
         twiddle = self.twiddle
         quit = self.quit
-
+        
         min_step = 10 * np.abs(np.nextafter(t, self.direction * np.inf) - t)
         if self.h_abs > self.max_step:
             h_abs = self.max_step
@@ -175,7 +183,7 @@ class CK45(RungeKutta):
             h_abs = min_step
         else:
             h_abs = self.h_abs
-
+        
         order_accepted = 0
         step_rejected = False
         while not order_accepted:
@@ -199,7 +207,7 @@ class CK45(RungeKutta):
             
             # first order error, second order solution, for assessment
             y_assess, err_assess, tol = self._comp_sol_err_tol(
-                                        h, self.B_assess[0], self.E_assess[0])
+                                    h, self.B_assess[0], self.E_assess[0], 2)
             E1 = norm(err_assess/tol)**(1/2)
             esttol = E1/self.quit[0]
             
@@ -211,7 +219,7 @@ class CK45(RungeKutta):
                 
                 # second order error, third order solution, for assessment
                 y_assess, err_assess, tol = self._comp_sol_err_tol(
-                                        h, self.B_assess[1], self.E_assess[1])
+                                    h, self.B_assess[1], self.E_assess[1], 4)
                 E2 = norm(err_assess/tol)**(1/3)
                 esttol = E2/self.quit[1]
                 
@@ -261,7 +269,7 @@ class CK45(RungeKutta):
                     # assess propagation with third order fallback solution
                     if E2 < 1:
                         y_new, err, tol = self._comp_sol_err_tol(
-                                    h, self.B_fallback[1], self.E_fallback[1])
+                                h, self.B_fallback[1], self.E_fallback[1], 4)
                         
                         # assess second order error
                         if norm(err/tol) < 1:
@@ -274,7 +282,7 @@ class CK45(RungeKutta):
                 # assess propagation with second order fallback solution
                 if E1 < 1:
                     y_new, err, tol = self._comp_sol_err_tol(
-                                    h, self.B_fallback[0], self.E_fallback[0])
+                                h, self.B_fallback[0], self.E_fallback[0], 2)
                     
                     # assess first order error
                     if norm(err/tol) < 1:
@@ -313,26 +321,21 @@ class CK45(RungeKutta):
         self.f = f_new
         return True, None
     
-    
     def _rk_stage(self, h, i):
         dy = np.dot(self.K[:i,:].T, self.A[i,:i]) * h
         self.K[i] = self.fun(self.t + self.C[i]*h, self.y + dy)
     
+    def _compute_error(self, h, E, i):
+        return h*np.dot(self.K[:i,:].T, E[:i])
     
-    def _compute_error(self, h, E):
-        return h*np.dot(self.K[:-1,:].T, E)
+    def _compute_solution(self, h, B, i):
+        return np.dot(self.K[:i,:].T, B[:i]) * h + self.y
     
-    
-    def _compute_solution(self, h, B):
-        return np.dot(self.K[:-1,:].T, B) * h + self.y
-    
-    
-    def _comp_sol_err_tol(self, h, B, E):
-        sol = self._compute_solution(h, B)
-        err = self._compute_error(h, E)
+    def _comp_sol_err_tol(self, h, B, E, i=6):
+        sol = self._compute_solution(h, B, i)
+        err = self._compute_error(h, E, i)
         tol = self.atol + self.rtol*np.maximum(np.abs(self.y), np.abs(sol))
         return sol, err, tol
-    
     
     def _dense_output_impl(self):
         # select interpolator based on order of the accepted error (solution)
@@ -343,3 +346,144 @@ class CK45(RungeKutta):
         return RkDenseOutput(self.t_old, self.t, self.y_old, Q)
 
 
+class CK45_o(CK45):
+    """As CK45, but fixed at 5th order solution porpagator with 4th order error 
+    estimator. (suffix _o for order)
+
+    Parameters
+    ----------
+    fun : callable
+        Right-hand side of the system. The calling signature is ``fun(t, y)``.
+        Here ``t`` is a scalar, and there are two options for the ndarray ``y``:
+        It can either have shape (n,); then ``fun`` must return array_like with
+        shape (n,). Alternatively it can have shape (n, k); then ``fun``
+        must return an array_like with shape (n, k), i.e., each column
+        corresponds to a single column in ``y``. The choice between the two
+        options is determined by `vectorized` argument (see below).
+    t0 : float
+        Initial time.
+    y0 : array_like, shape (n,)
+        Initial state.
+    t_bound : float
+        Boundary time - the integration won't continue beyond it. It also
+        determines the direction of the integration.
+    first_step : float or None, optional
+        Initial step size. Default is ``None`` which means that the algorithm
+        should choose.
+    max_step : float, optional
+        Maximum allowed step size. Default is np.inf, i.e., the step size is not
+        bounded and determined solely by the solver.
+    rtol, atol : float and array_like, optional
+        Relative and absolute tolerances. The solver keeps the local error
+        estimates less than ``atol + rtol * abs(y)``. Here `rtol` controls a
+        relative accuracy (number of correct digits). But if a component of `y`
+        is approximately below `atol`, the error only needs to fall within
+        the same `atol` threshold, and the number of correct digits is not
+        guaranteed. If components of y have different scales, it might be
+        beneficial to set different `atol` values for different components by
+        passing array_like with shape (n,) for `atol`. Default values are
+        1e-3 for `rtol` and 1e-6 for `atol`.
+    vectorized : bool, optional
+        Whether `fun` is implemented in a vectorized fashion. Default is False.
+
+    Attributes
+    ----------
+    n : int
+        Number of equations.
+    status : string
+        Current status of the solver: 'running', 'finished' or 'failed'.
+    t_bound : float
+        Boundary time.
+    direction : float
+        Integration direction: +1 or -1.
+    t : float
+        Current time.
+    y : ndarray
+        Current state.
+    t_old : float
+        Previous time. None if no steps were made yet.
+    step_size : float
+        Size of the last successful step. None if no steps were made yet.
+    nfev : int
+        Number evaluations of the system's right-hand side.
+    njev : int
+        Number of evaluations of the Jacobian. Is always 0 for this solver as 
+        it does not use the Jacobian.
+    nlu : int
+        Number of LU decompositions. Is always 0 for this solver.
+
+    References
+    ----------
+    .. [1] J. R. Cash, A. H. Karp, "A Variable Order Runge-Kutta Method for 
+           Initial Value Problems with Rapidly Varying Right-Hand Sides",
+           ACM Trans. Math. Softw., Vol. 16, No. 3, 1990, pp. 201-222, ISSN 
+           0098-3500. https://doi.org/10.1145/79505.79507
+    """
+    
+    max_factor = 10     # use scipy default
+    order_accepted = 4  # for dense output
+    
+    def _step_impl(self):
+        t = self.t
+        y = self.y
+        max_step = self.max_step
+        rtol = self.rtol
+        atol = self.atol
+        
+        min_step = 10 * np.abs(np.nextafter(t, self.direction * np.inf) - t)
+        if self.h_abs > max_step:
+            h_abs = max_step
+        elif self.h_abs < min_step:
+            h_abs = min_step
+        else:
+            h_abs = self.h_abs
+        
+        step_accepted = False
+        step_rejected = False
+        while not step_accepted:
+            if h_abs < min_step:
+                return False, self.TOO_SMALL_STEP
+            h = h_abs * self.direction
+            t_new = t + h
+            if self.direction * (t_new - self.t_bound) > 0:
+                t_new = self.t_bound
+            # added look ahead to prevent too small last step
+            elif abs(t_new - self.t_bound) <= min_step:
+                t_new = t + h/2
+            h = t_new - t
+            h_abs = np.abs(h)
+
+            # not FSAL, do last evaluation only after step is accepted
+            # calculate the 6 stages
+            self.K[0] = self.f                              # stage 0 (FSAL)
+            for i in range(1, 6):
+                self._rk_stage(h, i)                        # stages 1-5
+            y_new, err, tol = self._comp_sol_err_tol(h, self.B, self.E)
+            error_norm = norm(err/tol)
+            if error_norm < 1:
+                if error_norm == 0:
+                    factor = self.max_factor
+                else:
+                    factor = min(self.max_factor,
+                                 SAFETY * error_norm ** self.error_exponent)
+                if step_rejected:
+                    factor = min(1, factor)
+                h_abs *= factor
+                step_accepted = True
+                # now do the last evaluation
+                f_new = self.fun(t_new, y_new)
+                self.K[6] = f_new                               # stage 6 (FSAL)
+                
+            else:
+                h_abs *= max(MIN_FACTOR,
+                             SAFETY * error_norm ** self.error_exponent)
+                step_rejected = True
+        
+        self.h_previous = h
+        self.y_old = y
+        self.t = t_new
+        self.y = y_new
+        self.h_abs = h_abs
+        self.f = f_new
+        return True, None
+    
