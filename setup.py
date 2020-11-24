c8d54806f1dea44eab38f27b5e5e0d96bab14150
WRKampi <WRKampi@users.noreply.github.com>
2020-11-24 21:24:01 +0100

    several updates

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ setuptools.setup(
     version="0.0.1",
     author="W.R. Kampinga",
     author_email="",
-    description="extending solve_ivp of scipy integrate with OdeSolver objects",
+    description="extend solve_ivp of scipy.integrate with OdeSolver objects",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/WRKampi/extensisq",
     packages=setuptools.find_packages(),
     install_requires=[
-        "numpy",
+        "numpy>=1.4.0",
         "scipy>=1.0.0",
     ],
     classifiers=[
@@ -23,4 +23,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    tests_require=['pytest']
 )
