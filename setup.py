3568ceb75accc4552b7caccdbed24e6750bf6431
WRKampi <WRKampi@users.noreply.github.com>
2021-03-12 18:58:52 +0100

    version numbers

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="extensisq",
-    version="0.2.2",
+    version="0.2.3",
     author="W.R. Kampinga",
     author_email='wrkampi@tuta.io',
     description="Extend scipy.integrate with various methods for solve_ivp",
