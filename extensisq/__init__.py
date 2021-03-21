e8f8f19d3c3f4a9787c6100f9febfb47201097f2
WRKampi <WRKampi@users.noreply.github.com>
2021-03-21 21:19:44 +0100

    Update __init__.py

--- extensisq/__init__.py
+++ extensisq/__init__.py
@@ -7,10 +7,14 @@ from extensisq.cash import CK5, CKdisc, CK45, CK45_o
 from extensisq.prince import Pr7, Pr8, Pr9, Pri6, Pri7, Pri8
 from extensisq.shampine import SWAG
 from extensisq.common import NFS
+from extensisq.calvo import CFMR7osc
 
 __version__ = '0.2.3'
 __author__ = 'W.R. Kampinga'
 __copyright__ = 'Copyright 2020, W.R. Kampinga'
 __license__ = 'MIT'
-__credits__ = ('scipy', 'L.F Shampine', 'P. Bogacki', 'J.R. Cash', 'A.H. Karp',
-               'Ch. Tsitouras', 'P.J. Prince', 'H.A. Watts', 'M.K. Gordon')
+__credits__ = (
+    'scipy', 'L.F Shampine', 'P. Bogacki', 'J.R. Cash', 'A.H. Karp',
+    'Ch. Tsitouras', 'P.J. Prince', 'H.A. Watts', 'M.K. Gordon',
+    'G. Soederlind', 'K. Gustafsson', 'M. Calvo', 'J.M. Franco',
+    'J.I. Montijano', 'L. Randez')
