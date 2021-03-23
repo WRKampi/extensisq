2e29b8f07385b4c0ee44635d9d2ff0d6998f255f
WRKampi <WRKampi@users.noreply.github.com>
2021-03-23 23:31:54 +0100

    Update __init__.py

--- extensisq/__init__.py
+++ extensisq/__init__.py
@@ -1,13 +1,14 @@
 """This package extends scipy.integrate with various methods
 (OdeSolver classes) for the solve_ivp function.
 """
+from extensisq.common import NFS
 from extensisq.tsitouras import Ts5, Ts45
 from extensisq.bogacki import BS5, BS45, BS45_i
 from extensisq.cash import CK5, CKdisc, CK45, CK45_o
 from extensisq.prince import Pr7, Pr8, Pr9, Pri6, Pri7, Pri8
 from extensisq.shampine import SWAG
-from extensisq.common import NFS
 from extensisq.calvo import CFMR7osc
+from extensisq.sommeijer import SSV2stab
 
 __version__ = '0.2.3'
 __author__ = 'W.R. Kampinga'
@@ -17,4 +18,4 @@ __credits__ = (
     'scipy', 'L.F Shampine', 'P. Bogacki', 'J.R. Cash', 'A.H. Karp',
     'Ch. Tsitouras', 'P.J. Prince', 'H.A. Watts', 'M.K. Gordon',
     'G. Soederlind', 'K. Gustafsson', 'M. Calvo', 'J.M. Franco',
-    'J.I. Montijano', 'L. Randez')
+    'J.I. Montijano', 'L. Randez', 'B.P. Sommeijer', 'J.G. Verwer')
