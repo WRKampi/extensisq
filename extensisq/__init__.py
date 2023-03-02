8079accc3bf34c39de9c5cdfde335be28708d9dc
WRKampi <WRKampi@users.noreply.github.com>
2023-03-02 12:50:06 +0100

    update version

--- extensisq/__init__.py
+++ extensisq/__init__.py
@@ -13,7 +13,7 @@ from extensisq.merson import Me4
 from extensisq.sensitivity import (sens_forward, sens_adjoint_end,
                                    sens_adjoint_int)
 
-__version__ = '0.3.1'
+__version__ = '0.4.0'
 __author__ = 'W.R. Kampinga'
 __copyright__ = 'Copyright 2020, W.R. Kampinga'
 __license__ = 'MIT'
@@ -21,4 +21,5 @@ __credits__ = (
     'scipy', 'L.F Shampine', 'P. Bogacki', 'R.W. Brankin', 'I. Gladwell',
     'J.R. Cash', 'A.H. Karp', 'Ch. Tsitouras', 'P.J. Prince', 'H.A. Watts',
     'M.K. Gordon', 'G. Soederlind', 'K. Gustafsson', 'M. Calvo', 'J.M. Franco',
-    'J.I. Montijano', 'L. Randez', 'B.P. Sommeijer', 'J.G. Verwer')
+    'J.I. Montijano', 'L. Randez', 'B.P. Sommeijer', 'J.G. Verwer',
+    'E. Hairer', 'A.C. Hindmarsh', 'R. Serban', 'R.H. Merson')
